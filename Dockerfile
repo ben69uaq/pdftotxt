e7c7687ee8b6f4f26843a6b1bdd0ab14de1d05d7
ben69uaq <ben69uaq@gmail.com>
2024-01-18 22:14:20 +0100

    🐛 fixin build

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM azul/zulu-openjdk:17-latest
 COPY  app.jar app.jar
 EXPOSE 8080
-ENTRYPOINT ["java","-Droot=/tmp/data","-jar","/app.jar", ]
+ENTRYPOINT ["java","-Droot=/tmp/data","-jar","/app.jar"]
--- src/main/java/com/herokuapp/ApplicationConfiguration.java
+++ src/main/java/com/herokuapp/ApplicationConfiguration.java
@@ -3,7 +3,6 @@ package com.herokuapp;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
